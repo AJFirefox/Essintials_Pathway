3914096f3c31de9da1e1e6f1d565faeea1819b1d
AJFirefox <AJFirefox08@gmail.com>
2026-04-27 10:45:22 -0500

    Step 8

--- Assets/_Unity Essentials/Scripts/Collectible.cs
+++ Assets/_Unity Essentials/Scripts/Collectible.cs
@@ -5,6 +5,8 @@ public class Collectible : MonoBehaviour
 
     public float rotationSpeed;
 
+    public GameObject onCollectEffect;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,4 +20,15 @@ public class Collectible : MonoBehaviour
     { 
         transform.Rotate(0, rotationSpeed, 0);
     }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Destroy the collectible
+        Destroy(gameObject);
+
+        //instantiate the particle effect
+        Instantiate(onCollectEffect, transform.position, transform.rotation);
+    }
+
 }
